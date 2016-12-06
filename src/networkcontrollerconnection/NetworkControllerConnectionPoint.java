655451b4deb0b9ac492c504cf22eb4ba6c6daf02
Jorge Cano <jacano.sosa@gmail.com>
2016-12-06 17:06:25 -0600

    Starting point

---
+++ src/networkcontrollerconnection/NetworkControllerConnectionPoint.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package networkcontrollerconnection;
+
+/**
+ *
+ * @author jorge
+ */
+public class NetworkControllerConnectionPoint {
+    
+    private final String IP = "192.168.69.18";
+    private final String USERNAME = "rys2016";
+    private final String PASSWORD = "2016RyS";
+    
+    private final String ADD_DEVICE_COMMAND = "config macfilter add";
+    private final String WIFI_NAME = "wifi-rys2016-d";
+    private final String WIFI_DESCRIPTION = "\"Red WIFI D RyS 2016\"";
+    
+    private final SSHManager ConnectionManager = new SSHManager(this.USERNAME, this.PASSWORD, this.IP, "");
+
+    public NetworkControllerConnectionPoint() {
+        
+        this.ConnectionManager.connect();
+    }
+    
+    public void registerMACAddress(String MACAddressToRegister){
+        
+        this.ConnectionManager.sendCommand(
+            this.constructAddMACCommand( MACAddressToRegister )
+        );
+    }
+
+    private String constructAddMACCommand(String MACAddressToRegister) {
+        
+        return this.ADD_DEVICE_COMMAND + " " +
+                MACAddressToRegister + " " +
+                this.WIFI_NAME + " " +
+                this.WIFI_DESCRIPTION;
+    }
+    
+    public void close(){
+        
+        this.ConnectionManager.close();
+    }
+}
