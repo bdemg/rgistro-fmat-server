a512b55aca29b582fd495a4f06c2f68f393b1591
Jorge Cano <jacano.sosa@gmail.com>
2016-12-08 16:01:30 -0600

    Implementación de los métodos del DAO

--- src/dao/MACDevicesDAO.java
+++ src/dao/MACDevicesDAO.java
@@ -5,6 +5,8 @@
  */
 package dao;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -14,6 +16,7 @@ import java.sql.SQLException;
 public class MACDevicesDAO extends DatabaseDAO {
     
     private final String CHECK_USER_DEVICES_QUERY = "SELECT deviceMAC FROM devices WHERE matricula=?";
+    private final String CHECK_FOR_USER = "SELECT matricula FROM devices WHERE matricula=?";
     private final String ADD_USER_DEVICE_QUERY = "INSERT INTO devices VALUES (?, ?)";
     private final String REMOVE_USER_DEVICE_QUERY = "DELETE FROM devices WHERE deviceMAC=?";
 
@@ -22,5 +25,53 @@ public class MACDevicesDAO extends DatabaseDAO {
     }
     
     
+    public boolean isUserRegistered(String userRegisterNumber) throws SQLException{
+        
+        PreparedStatement queryStatement = (PreparedStatement) 
+            super.connectionToDatabase.prepareStatement( this.CHECK_FOR_USER );
+        
+        queryStatement.setString(1, userRegisterNumber);
+        
+        ResultSet queryResults = queryStatement.executeQuery();
+        
+        boolean  isRegistered = queryResults.last();
+        return isRegistered;
+    }
     
+    
+    public int numberOfDevicesRegistered(String userRegisterNumber) throws SQLException{
+        
+        PreparedStatement queryStatement = (PreparedStatement) 
+            super.connectionToDatabase.prepareStatement( this.CHECK_USER_DEVICES_QUERY );
+        
+        queryStatement.setString(1, userRegisterNumber);
+        
+        ResultSet queryResults = queryStatement.executeQuery();
+        
+        queryResults.last();
+        return queryResults.getRow();
+    }
+    
+    
+    public void addUserDevice(String userRegisterNumber, String deviceMAC) throws SQLException{
+        
+        PreparedStatement queryStatement = (PreparedStatement) 
+            super.connectionToDatabase.prepareStatement( this.ADD_USER_DEVICE_QUERY );
+        
+        queryStatement.setString(1, userRegisterNumber);
+        queryStatement.setString(2, deviceMAC);
+        
+        queryStatement.execute();
+    }
+    
+    
+    public void removeUserDevice(String deviceMAC) throws SQLException{
+        
+        PreparedStatement queryStatement = (PreparedStatement) 
+            super.connectionToDatabase.prepareStatement( this.REMOVE_USER_DEVICE_QUERY );
+        
+        queryStatement.setString(1, deviceMAC);
+        
+        queryStatement.execute();
+    }
 }
