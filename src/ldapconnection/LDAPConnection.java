6b9158030173b2408a1331629e3a220134a0a6ee
Mariana Rocha <mnrt.cr@gmail.com>
2016-12-10 18:49:59 -0600

    Agregada búsqueda ldap por usuario

--- src/ldapconnection/LDAPConnection.java
+++ src/ldapconnection/LDAPConnection.java
@@ -1,12 +1,13 @@
 package ldapconnection;
 
 import java.util.Hashtable;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.naming.Context;
+import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
 import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
 
 /**
  * Example code for retrieving a Users Primary Group from Microsoft Active
@@ -18,6 +19,8 @@ public class LDAPConnection {
 
     private final String ldapAdServer = "ldap://148.209.67.42:389";
     private final String ldapSearchBase = "ou=Facultad de Matematicas, ou=INET, dc=inet, dc=uady, dc=mx";
+    private String email = "";
+    private String username = "";
 
     public boolean searchUser(String ldapUsername, String ldapPassword) {
 
@@ -25,27 +28,61 @@ public class LDAPConnection {
         env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
         env.put(Context.PROVIDER_URL, ldapAdServer);
         env.put(Context.SECURITY_AUTHENTICATION, "simple");
+
+        String ldapUsernameCopy= ldapUsername;
         
         if (ldapUsername != null) {
             env.put(Context.SECURITY_PRINCIPAL,
-                    "cn=" + ldapUsername +", " + ldapSearchBase);
+                    "cn=" + ldapUsername + ", " + ldapSearchBase);
         }
-        
+
         if (ldapPassword != null) {
             env.put(Context.SECURITY_CREDENTIALS, ldapPassword);
         }
 
         try {
 
-            DirContext contex = new InitialDirContext(env);
-            contex.close();
-            
+            DirContext context = new InitialDirContext(env);
+            search(ldapUsernameCopy, context);
+
+            context.close();
+
             return true;
         } catch (NamingException ex) {
-            
+
             ex.printStackTrace();
             return false;
         }
+    }
 
+    private void search(String ldapUsername, DirContext context) throws NamingException {
+        //Recordar que el username tiene que tener toda la ruta de OUs/DCs/CNs
+        SearchControls control = new SearchControls();
+        control.setReturningObjFlag(true); // Para que devuelva los elementos que buscamos
+
+        String returning[] = new String[2];
+        returning[0] = "field1";
+        returning[1] = "fields2";
+        //Asignamos los atributos a devolver
+        control.setReturningAttributes(returning);
+        control.setSearchScope(SearchControls.OBJECT_SCOPE);
+
+        String search = ldapUsername; // Search es "en donde buscar" de los directorios del servidor
+        String filter = "(sAMAccountName=" + ldapUsername + ")"; // filtro trivial
+
+        NamingEnumeration answer = context.search(search, filter, control);
+        SearchResult result = (SearchResult) answer.next(); // Sabemos que habra un solo resultado
+
+        email = result.getAttributes().get("field1").get().toString();      //Buscar nombre de columna
+        username = result.getAttributes().get("field2").get().toString();   //para correo y username
+//        System.out.println(ldapUsername + ": " + email + ", " + username);
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
     }
 }
